18a5611d3927fee0b2b71025630877ab2d299707
nayan <wsnayan6@gmail.com>
2025-06-23 10:29:46 +0600

    sdaf

--- school/resources/views/auth/register.blade.php
+++ school/resources/views/auth/register.blade.php
@@ -39,6 +39,13 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
+        <div class="form-group">
+    <label for="role">Register As</label>
+    <select name="role" class="form-control" required>
+        <option value="user">User</option>
+        <option value="admin">Admin</option>
+    </select>
+</div>
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
                 {{ __('Already registered?') }}

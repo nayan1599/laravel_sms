3fe6b6351df7aae24d9c9ed2f009f29b57273329
Your Name <you@example.com>
2026-03-09 20:58:47 +0600

    sdf

--- school/resources/js/app.js
+++ school/resources/js/app.js
@@ -1,7 +1,5 @@
-import './bootstrap';
+// resources/js/bootstrap.js
+import axios from 'axios';
+window.axios = axios;
 
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
